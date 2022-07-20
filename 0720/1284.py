852066eeb2081acd7296074833ab289ff8ff0fa4
kfrrsi0301 <kfrrsi0301@naver.com>
2022-07-20 17:43:50 +0900

    0720실습

--- 0720/1284.py
+++ 0720/1284.py
@@ -5,9 +5,24 @@ t = int(input())
 
 for i in range(1, t+1):
     P, Q, R, S, W = map(int,input().split())
-    a_case, b_case = 0, 0
-    a_case += W*P
-    b_case += Q
-    if W > R:
-        b_case += (W-R)*S
-print('#{} {}'.format(i, min(a_case, b_case)))
+    a = W*P
+    if R > W:
+        b = Q
+    else:
+        b = Q + S*(W-R)
+    print(f'#{i} {min(a, b)}')
+    
+
+# 오류났던 코드
+# t = int(input())
+
+# for i in range(1, t+1):
+#     P, Q, R, S, W = map(int,input().split())
+#     a_case, b_case = 0, 0
+#     a_case += W*P
+#     b_case += Q
+#     if W > R:
+#         b_case += (W-R)*S
+# print('#{} {}'.format(i, min(a_case, b_case)))
+
+
--- 0720/2050.py
+++ 0720/2050.py
@@ -5,4 +5,4 @@ sys.stdin = open("2050_input.txt", "r")
 a =input()
 for i in a:
     n = ord(i)-64
-    print(n, end ='')
+    print(n, end =' ')
